cc00e7bedc86e2deb51fdf614d216d463e0f89de
David Ruisinger <david.ruisinger@gmail.com>
2016-10-18 18:06:31 +0200

    Implements Alipay Payment for DONGXii

--- ios/RNAlipay/Order.m
+++ ios/RNAlipay/Order.m
@@ -1,56 +1,68 @@
+//
+//  Order.m
+//  DONGXii Alipay integration
+//
+//  Created by David Ruisinger on 17.10.16.
+//  Copyright © 2016 Things Move China GmbH. All rights reserved.
+//
+
 #import "Order.h"
 
 @implementation Order
 
 - (NSString *)description {
-  NSMutableString * discription = [NSMutableString string];
-  if (self.partner) {
-    [discription appendFormat:@"partner=\"%@\"", self.partner];
-  }
-  
-  if (self.sellerID) {
-    [discription appendFormat:@"&seller_id=\"%@\"", self.sellerID];
-  }
-  if (self.outTradeNO) {
-    [discription appendFormat:@"&out_trade_no=\"%@\"", self.outTradeNO];
-  }
-  if (self.subject) {
-    [discription appendFormat:@"&subject=\"%@\"", self.subject];
-  }
-  
-  if (self.body) {
-    [discription appendFormat:@"&body=\"%@\"", self.body];
-  }
-  if (self.totalFee) {
-    [discription appendFormat:@"&total_fee=\"%@\"", self.totalFee];
-  }
-  if (self.notifyURL) {
-    [discription appendFormat:@"&notify_url=\"%@\"", self.notifyURL];
-  }
-  
-  if (self.service) {
-    [discription appendFormat:@"&service=\"%@\"",self.service];//mobile.securitypay.pay
-  }
-  if (self.paymentType) {
-    [discription appendFormat:@"&payment_type=\"%@\"",self.paymentType];//1
-  }
-  
-  if (self.inputCharset) {
-    [discription appendFormat:@"&_input_charset=\"%@\"",self.inputCharset];//utf-8
-  }
-  if (self.itBPay) {
-    [discription appendFormat:@"&it_b_pay=\"%@\"",self.itBPay];//30m
-  }
-  if (self.showURL) {
-    [discription appendFormat:@"&show_url=\"%@\"",self.showURL];//m.alipay.com
-  }
-  if (self.appID) {
-    [discription appendFormat:@"&app_id=\"%@\"",self.appID];
-  }
-  for (NSString * key in [self.outContext allKeys]) {
-    [discription appendFormat:@"&%@=\"%@\"", key, [self.outContext objectForKey:key]];
-  }
-  return discription;
+    
+    NSMutableString * discription = [NSMutableString string];
+    
+    if (self.partner) {
+        [discription appendFormat:@"partner=\"%@\"", self.partner];
+    }
+    if (self.seller) {
+        [discription appendFormat:@"&seller_id=\"%@\"", self.seller];
+    }
+    if (self.out_trade_no) {
+        [discription appendFormat:@"&out_trade_no=\"%@\"", self.out_trade_no];
+    }
+    if (self.subject) {
+        [discription appendFormat:@"&subject=\"%@\"", self.subject];
+    }
+    if (self.body) {
+        [discription appendFormat:@"&body=\"%@\"", self.body];
+    }
+    if (self.rmb_fee) {
+        [discription appendFormat:@"&rmb_fee=\"%@\"", self.rmb_fee];
+    }
+    if (self.currency) {
+        [discription appendFormat:@"&currency=\"%@\"", self.currency];
+    }
+    if (self.forex_biz) {
+        [discription appendFormat:@"&forex_biz=\"%@\"", self.forex_biz];
+    }
+    if (self.notifyURL) {
+        [discription appendFormat:@"&notify_url=\"%@\"", self.notifyURL];
+    }
+    if (self.service) {
+        [discription appendFormat:@"&service=\"%@\"",self.service];
+    }
+    if (self.payment_type) {
+        [discription appendFormat:@"&payment_type=\"%@\"",self.payment_type];
+    }
+    if (self._input_charset) {
+        [discription appendFormat:@"&_input_charset=\"%@\"",self._input_charset];
+    }
+    if (self.it_b_pay) {
+        [discription appendFormat:@"&it_b_pay=\"%@\"",self.it_b_pay];
+    }
+    if (self.show_url) {
+        [discription appendFormat:@"&show_url=\"%@\"",self.show_url];
+    }
+    if (self.sign_date) {
+        [discription appendFormat:@"&sign_date=\"%@\"",self.sign_date];
+    }
+    if (self.app_id) {
+        [discription appendFormat:@"&app_id=\"%@\"",self.app_id];
+    }
+    return discription;
 }
 
 
--- ios/RNAlipay/RNAlipay.m
+++ ios/RNAlipay/RNAlipay.m
@@ -12,99 +12,63 @@ RCT_REMAP_METHOD(pay, options:(NSDictionary *)options
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
     
-    /*
-     *商户的唯一的parnter和seller。
-     *签约后，支付宝会为每个商户分配一个唯一的 parnter 和 seller。
-     */
+    NSString *privateKey = @"__FILL IN HERE__";
     
-    /*============================================================================*/
-    /*=======================需要填写商户app申请的===================================*/
-    /*============================================================================*/
-    NSString *partner = [options objectForKey:@"partner"];
-    NSString *seller = [options objectForKey:@"seller"];
-    NSString *privateKey = [options objectForKey:@"privateKey"];
-    /*============================================================================*/
-    /*============================================================================*/
-    /*============================================================================*/
+    NSString *appScheme = @"com.dongxii.alipay";
     
-    //partner和seller获取失败,提示
-    if ([partner length] == 0 ||
-        [seller length] == 0 ||
-        [privateKey length] == 0)
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
-                                                        message:@"缺少partner或者seller或者私钥。"
-                                                       delegate:self
-                                              cancelButtonTitle:@"确定"
-                                              otherButtonTitles:nil];
-        [alert show];
-        
-        NSError *error = nil;
-        reject(@"错误", @"缺少partner或者seller或者私钥。", error);
-        
-        return;
-    }
-    
-    /*
-     *生成订单信息及签名
-     */
-    //将商品信息赋予AlixPayOrder的成员变量
+    // Create the order
     Order *order = [[Order alloc] init];
-    order.partner = partner;
-    order.sellerID = seller;
-    order.outTradeNO = [options objectForKey:@"outTradeNO"]; //订单ID（由商家自行制定）
-    order.subject = [options objectForKey:@"subject"]; //商品标题
-    order.body = [options objectForKey:@"body"]; //商品描述
-    
-    float totalFee = [[options objectForKey:@"totalFee"] floatValue];
-    order.totalFee = [NSString stringWithFormat:@"%0.2f", totalFee]; //商品价格
-    order.notifyURL =  [options objectForKey:@"notifyURL"]; //回调URL
-    
+    order.partner = @"__FILL IN HERE__";
+    order.seller = @"__FILL IN HERE__";
+    order.out_trade_no = [options objectForKey:@"orderId"];
+    order.subject = [options objectForKey:@"subject"];
+    order.body = [options objectForKey:@"body"];
+    float rmb_fee = [[options objectForKey:@"amount"] floatValue];
+    order.rmb_fee = [NSString stringWithFormat:@"%0.2f", rmb_fee];
+    order.currency = @"__FILL IN HERE__";
+    order.forex_biz = @"FP";
+    order.notifyURL =  @"__FILL IN HERE__";
     order.service = @"mobile.securitypay.pay";
-    order.paymentType = @"1";
-    order.inputCharset = @"utf-8";
+    order.payment_type = @"1";
+    order._input_charset = @"utf-8";
+    order.it_b_pay = @"30m";
+    order.show_url = @"m.alipay.com";
     
-    order.itBPay = [options objectForKey:@"itBPay"];
-    order.showURL = [options objectForKey:@"showURL"];
     
-    //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
-    NSString *appScheme = [options objectForKey:@"appSchemeIOS"];
+    NSLog(@"ALIPAY: Order = %@", order.out_trade_no);
+    NSLog(@"ALIPAY: Subject = %@", order.subject);
+    NSLog(@"ALIPAY: Body = %@", order.body);
+    NSLog(@"ALIPAY: Amount = %@", order.rmb_fee);
     
-    //将商品信息拼接成字符串
+    // Create the orderString
     NSString *orderSpec = [order description];
-    //NSLog(@"orderSpec = %@", orderSpec);
-    
-    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
+    NSLog(@"ALIPAY: orderSpec = %@",orderSpec);
+
+    // Sign the orderString
     id<DataSigner> signer = CreateRSADataSigner(privateKey);
     NSString *signedString = [signer signString:orderSpec];
     
-    //将签名成功字符串格式化为订单字符串,请严格按照该格式
+    NSLog(@"ALIPAY: signedString = %@",signedString);
+    
+
+    
+    // Initiate the payment
     NSString *orderString = nil;
     if (signedString != nil) {
         orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
                        orderSpec, signedString, @"RSA"];
-        NSLog(@"orderString = %@", orderString);
-        
-        //resolve(@"支付成功!");
         
         [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
-            NSLog(@"reslut = %@",resultDic);
-            
-            NSLog(@"orderString = %@", @"支付成功啦啦啦啦！");
-            resolve(@"支付成功!");
+            NSString *status = resultDic[@"resultStatus"];
+            if ([status integerValue] >= 8000) {
+                resolve(@[resultDic]);
+            } else {
+                reject(status, resultDic[@"memo"], [NSError errorWithDomain:resultDic[@"memo"] code:[status integerValue] userInfo:NULL]);
+            }
         }];
-        return;
+        
+        
     }
-    
-    NSError *error = nil;
-    reject(@"支付失败", @"参数错误", error);
-    
-//    if (str) {
-//        resolve(str);
-//    } else {
-//        NSError *error = nil;
-//        reject(@"no_events", @"There were no events", error);
-//    }
 }
 
 @end
